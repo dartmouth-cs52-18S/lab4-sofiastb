a662460b69ef8d2645f7e1f95a9e6269ebb50583
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-04-13 15:30:19 -0400

    Add image-webpack-loader to compress and optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,31 @@ module.exports = {
               name: '[name].[ext]',
             },
           },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
         ],
       },
     ],
