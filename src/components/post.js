050562df87b1e19a9a45f37ae48eddbbe15b404b
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-05-01 11:10:03 -0400

    Got deletePost to work

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 
 class Post extends Component {
   constructor(props) {
@@ -12,6 +12,8 @@ class Post extends Component {
       // coverImage: '',
       // content: '',
     };
+
+    this.delete = this.delete.bind(this);
   }
 
   // getting the id was done usint this StackOverflow post: https://stackoverflow.com/questions/45069824/how-to-pass-data-through-my-react-router-with-reactjs
@@ -19,13 +21,30 @@ class Post extends Component {
     this.props.fetchPost(this.props.match.params.postID);
   }
 
+  delete(event) {
+    event.preventDefault();
+    this.props.deletePost(this.props.match.params.postID, this.props.history);
+  }
+
   render() {
     return (
-      <div className="singular-post">
-        <img src={this.props.selectedPost.cover_url} alt={this.props.selectedPost.title} />
-        <h1>{this.props.selectedPost.title}</h1>
-        <h2>{this.props.selectedPost.tags}</h2>
-        <p>{this.props.selectedPost.content}</p>
+      <div id="post-view">
+        <div className="singular-post">
+          <img src={this.props.selectedPost.cover_url} alt={this.props.selectedPost.title} />
+          <h1>{this.props.selectedPost.title}</h1>
+          <h2>{this.props.selectedPost.tags}</h2>
+          <p>{this.props.selectedPost.content}</p>
+        </div>
+        <div id="controls">
+          <span
+            alt="delete"
+            role="button"
+            tabIndex={-1}
+            onClick={this.delete}
+            className="lnr lnr-trash"
+          />
+          <span alt="edit" className="lnr lnr-pencil" />
+        </div>
       </div>
     );
   }
@@ -36,4 +55,4 @@ const mapStateToProps = state => ({
   selectedPost: state.posts.post,
 });
 
-export default withRouter(connect(mapStateToProps, { fetchPost })(Post));
+export default withRouter(connect(mapStateToProps, { fetchPost, deletePost })(Post));
