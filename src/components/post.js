b8aae50725acb152a1e73ea01e0924d2a544d407
Sofia Stanescu-Bellu <sofiastb27@gmail.com>
2018-05-14 23:13:19 -0400

    Changes to fix errors

--- src/components/post.js
+++ src/components/post.js
@@ -22,7 +22,6 @@ class Post extends Component {
       localContent: '',
       localTags: '',
       authed: localStorage.getItem('token'),
-      currAuthor: (JSON.parse(localStorage.getItem('user')).id === this.props.selectedPost.author),
     };
 
     // cover photo
@@ -76,28 +75,28 @@ class Post extends Component {
   // set editing status
   editCoverPhoto(event) {
     event.preventDefault();
-    if (!this.state.isEditingCoverPhoto && this.state.authed && this.state.currAuthor) {
+    if (!this.state.isEditingCoverPhoto && this.state.authed && (JSON.parse(localStorage.getItem('user')).id === this.props.selectedPost.author)) {
       this.setState({ isEditingCoverPhoto: true, localCoverPhoto: this.props.selectedPost.cover_url });
     }
   }
 
   editTitle(event) {
     event.preventDefault();
-    if (!this.state.isEditingTitle && this.state.authed && this.state.currAuthor) {
+    if (!this.state.isEditingTitle && this.state.authed && (JSON.parse(localStorage.getItem('user')).id === this.props.selectedPost.author)) {
       this.setState({ isEditingTitle: true, localTitle: this.props.selectedPost.title });
     }
   }
 
   editContent(event) {
     event.preventDefault();
-    if (!this.state.isEditingContent && this.state.authed && this.state.currAuthor) {
+    if (!this.state.isEditingContent && this.state.authed && (JSON.parse(localStorage.getItem('user')).id === this.props.selectedPost.author)) {
       this.setState({ isEditingContent: true, localContent: this.props.selectedPost.content });
     }
   }
 
   editTags(event) {
     event.preventDefault();
-    if (!this.state.isEditingTags && this.state.authed && this.state.currAuthor) {
+    if (!this.state.isEditingTags && this.state.authed && (JSON.parse(localStorage.getItem('user')).id === this.props.selectedPost.author)) {
       this.setState({ isEditingTags: true, localTags: this.props.selectedPost.tags });
     }
   }
@@ -255,7 +254,6 @@ class Post extends Component {
 // connects particular parts of redux state to this components props
 const mapStateToProps = state => ({
   selectedPost: state.posts.post,
-  authedUser: state.auth.user,
 });
 
 export default withRouter(connect(mapStateToProps, { fetchPost, deletePost, updatePost })(onClickOutside(Post)));
